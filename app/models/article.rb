c5b32d8a60eb709679d20e8c262cad29bec9cc47
Avneesh Kohli <avneeshk91@gmail.com>
2012-03-14 18:59:34 -0700

    merge article model code

--- app/models/article.rb
+++ app/models/article.rb
@@ -466,4 +466,13 @@ class Article < Content
     to = to - 1 # pull off 1 second so we don't overlap onto the next day
     return from..to
   end
+  
+  def merge_articles(article_id)
+    article1 = self
+    article2 = Article.find_by_id(article_id)
+    
+    new_article = Article.get_or_build_article
+    new_article.author = article1.author
+    new_article.body = article1.body + '\n\n' + article2.body
+  end
 end
